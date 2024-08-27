248d86112582d124dbdd875ba9f90671ca3a8ff0
mohamed el merjani <mohamedelmerjani@gmail.com>
2024-08-27 23:54:43 +0100

    Install Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import config from "eslint-config-standard";
-
-
 export default [
-  ...[].concat(config),
+  {
+      rules: {
+          "no-unused-vars": "error",
+          "no-undef": "error"
+      }
+  }
 ];
